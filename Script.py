ef705c77c9b88915162bf8390e5d414ee56fc8a1
anshk2354 <155858616+anshk2354@users.noreply.github.com>
2025-05-03 20:44:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -265,7 +265,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/mxmoder">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/mxmoder>[mxmoder]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/mxmoder">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/mxmoder">[@mxmoder]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
